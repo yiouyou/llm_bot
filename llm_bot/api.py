4a3ac571568748020bd11e2ce4d16a981ae26a29
yiouyou <zhuosong@gmail.com>
2024-03-10 01:30:15 +0800

    0309

--- llm_bot/api.py
+++ llm_bot/api.py
@@ -14,6 +14,9 @@ def get_chatbot_response(session_id: str, prompt_message: str) -> str:
     mistral_api_key = frappe.conf.get("mistral_api_key")
     if not mistral_api_key:
         frappe.throw("Please set `mistral_api_key` in site config")
+    print(mistral_api_key)
+    llm = ChatMistralAI(mistral_api_key=mistral_api_key, model="mistral-large-latest", temperature=0)
+    ### RedisChatMessageHistory
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system", "你是一个AI助手。让我们一步步思考。"),
@@ -21,7 +24,7 @@ def get_chatbot_response(session_id: str, prompt_message: str) -> str:
             ("human", "{question}"),
         ]
     )
-    chain = prompt | ChatMistralAI(model_name=mistral_api_key, model="mistral-large-latest", temperature=0)
+    chain = prompt | llm
     chain_with_history = RunnableWithMessageHistory(
         chain,
         lambda session_id: RedisChatMessageHistory(
@@ -33,7 +36,19 @@ def get_chatbot_response(session_id: str, prompt_message: str) -> str:
     )
     config = {"configurable": {"session_id": session_id}}
     response = chain_with_history.invoke({"question": prompt_message}, config=config)
+    ### no RedisChatMessageHistory
+    # messages = [
+    #     SystemMessage(
+    #         content="你是一个AI助手。让我们一步步思考。"
+    #     ),
+    #     HumanMessage(
+    #         content=prompt_message
+    #     ),
+    # ]
+    # response = llm.invoke(messages)
+    print(response.content)
     return response.content
+
 
 
 def get_openai_model_from_settings():

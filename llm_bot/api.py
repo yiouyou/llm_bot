3c3a0ccda652d68eb151fcfee87f435fb90c8bef
yiouyou <zhuosong@gmail.com>
2024-03-10 20:09:26 +0800

    0309

--- llm_bot/api.py
+++ llm_bot/api.py
@@ -12,15 +12,19 @@ from langchain_community.chat_message_histories import RedisChatMessageHistory
 
 @frappe.whitelist()
 def get_chatbot_response(session_id: str, prompt_message: str) -> str:
-    mistral_api_key = frappe.conf.get("mistral_api_key")
-    if not mistral_api_key:
-        frappe.throw("Please set `mistral_api_key` in site config")
-    # print(mistral_api_key)
-    llm = ChatMistralAI(mistral_api_key=mistral_api_key, model="mistral-large-latest", temperature=0)
+    llm_type = get_llm_type_from_settings()
+    if llm_type == 'Mistral AI':
+        mistral_api_key = frappe.conf.get("mistral_api_key")
+        if not mistral_api_key:
+            frappe.throw("Please set `mistral_api_key` in site config")
+        # print(mistral_api_key)
+        llm = ChatMistralAI(mistral_api_key=mistral_api_key, model="mistral-large-latest", temperature=0)
+    else:
+        return f"{llm_type} is not supported yet."
     ### RedisChatMessageHistory
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "你是一个AI助手。让我们一步步思考。"),
+            ("system", "你是一个AI助手，你的名字是'小Yo'，你的生日是2024年2月29日。在帮助用户解决问题时，请一步步思考。在回答问题时，如果用户提供的信息不完整，请有礼貌地向用户提问，并提供提问的理由，以获得解决问题所需的必要信息。回复时，请务必不要说类似'让我们一步步思考'类似的话，请尽量以人类口吻自然的回答或提问。"),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{question}"),
         ]
@@ -47,13 +51,13 @@ def get_chatbot_response(session_id: str, prompt_message: str) -> str:
     #     ),
     # ]
     # response = llm.invoke(messages)
-    print(session_id, prompt_message)
-    print(session_id, response.content)
+    print(session_id, '[user]', prompt_message)
+    print(session_id, '[bot]', response.content)
     return response.content
 
 
 
-def get_openai_model_from_settings():
+def get_llm_type_from_settings():
     return (
-        frappe.db.get_single_value("LLM Bot Settings", "openai_model") or "gpt-3.5-turbo"
+        frappe.db.get_single_value("LLM Bot Settings", "llm_type") or "MistralAI"
     )

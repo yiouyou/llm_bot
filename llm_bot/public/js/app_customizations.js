5406df5a915a90ff6a6e70ce6809a369a3d0cb8a
yiouyou <zhuosong@gmail.com>
2024-03-11 09:59:44 +0800

    0311

--- llm_bot/public/js/app_customizations.js
+++ llm_bot/public/js/app_customizations.js
@@ -1,8 +1,8 @@
 frappe.ui.keys.add_shortcut({
   shortcut: "shift+ctrl+d",
   action: function () {
-    // navigate to ask llm bot page
-    frappe.set_route("llm-bot");
+    // navigate to ask llm page
+    frappe.set_route("ask-llm");
   },
-  description: __("Ask llmBot"),
+  description: __("Ask LLM"),
 });
